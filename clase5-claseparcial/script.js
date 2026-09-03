282007068db79fc3f51f30e04200cc5e2cd7cb90
Gonza <gonzalo.castelnuovvo@gmail.com>
2026-09-03 14:31:02 -0300

    mgomrpg

--- clase5-claseparcial/script.js
+++ clase5-claseparcial/script.js
@@ -3,3 +3,8 @@
 //addeventlistener para que se qude escuhando el boton
 //aca estamos haciendo el backoffice del sistema, no es como para el consumidor final, si lo fuese tendrias que tener el tachito de borrar y eso...
 // en el html cuando se te hace medio desordenado tocas click derecho, format document y te ordena las etiquetas
+//variables locale svs globales, a veces podes reusar lo que escribsite adentro d euna fucnoion pq son variables locales pq solo las usas ahi enteonces no se te mezcla
+//tocas una palabra y dsps escribis esto: ctrl+d y te selecciona muchos abajo o algo asi
+//con este escribis en muchas filas al mismo tiempo: alt+shift ensu compu no se como sera aca
+//<br> poner el salto de lineas
+//ponerle la linita que se mueve horizontalmente con le tema del descuento
