1a49630129ea48e34cac9eb63935a8855afdfcf3
Gonza <gonzalo.castelnuovvo@gmail.com>
2026-09-03 14:00:17 -0300

    d

---
+++ clase5-claseparcial/script.js
@@ -0,0 +1,5 @@
+//get element by id y get element by class se resumen en querySelection
+//eso es para agarrar lo de html en el js
+//addeventlistener para que se qude escuhando el boton
+//aca estamos haciendo el backoffice del sistema, no es como para el consumidor final, si lo fuese tendrias que tener el tachito de borrar y eso...
+// en el html cuando se te hace medio desordenado tocas click derecho, format document y te ordena las etiquetas
